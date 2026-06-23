124ff301d17a623202c7409e5ccec41107b226c3
pythonguylol <jacobindustries13@gmail.com>
2026-06-23 00:33:54 -0400

    Add LauncherConfig model

---
+++ Models/LauncherConfig.cs
@@ -0,0 +1,9 @@
+namespace HeardGreenLauncher.Models;
+
+public class LauncherConfig
+{
+    public string ServerName { get; } = "HeardGreen SMP";
+    public string ServerIP { get; } = "hg.sq3o.com";
+    public int MaxMemoryGB { get; } = 2;
+    public int MinMemoryGB { get; } = 1;
+}
